452ec130746fce9cd0e0be0d0c16d4b40b86fcef
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2023-01-16 18:47:56 +0000

    fix: container repo
    add container repo url while container build
    
    Signed-off-by: Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>

--- container.sh
+++ container.sh
@@ -5,6 +5,7 @@ source ./project.sh
 TARGET_HOST=$(echo ${CONTAINER_HOST}|sed 's-tcp://--g'|cut -f1 -d:)
 REV=$(git describe --long --tags --match='v*' --dirty 2>/dev/null || echo dev)
 
+CONTAINERREPO=${CONTAINERREPO:-localhost}
 
-podman build -f container/build.Containerfile -t ${USER}/${PROJECT}:$REV . ||exit 1
-podman tag ${USER}/${PROJECT}:$REV ${USER}/${PROJECT}:dev-latest
+podman build -f container/build.Containerfile -t ${CONTAINERREPO}/${REPOUSER}/${REPOPROJECT}:$REV . ||exit 1
+podman tag ${CONTAINERREPO}/${REPOUSER}/${PROJECT}:$REV ${CONTAINERREPO}/${REPOUSER}/${REPOPROJECT}:dev-latest
